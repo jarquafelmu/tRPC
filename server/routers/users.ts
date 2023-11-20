838d3bdc1b47f98e888e01a6ac2ac4bd29f79653
Sam Terrazas <thechaoselement@gmail.com>
2023-11-20 14:01:23 -0700

    composite procedures

--- server/routers/users.ts
+++ server/routers/users.ts
@@ -5,4 +5,13 @@ const userProcedure = t.procedure.input(z.object({ userId: z.string() }));
 
 export const userRouter = t.router({
   get: userProcedure.query(({ input }) => ({ id: input.userId })),
+  // Add an update mutation to the user router using a composite procedure
+  update: userProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation((req) => {
+      console.log(
+        `Updating user ${req.input.userId} to have the name ${req.input.name}`
+      );
+      return { id: req.input.userId, name: req.input.name };
+    }),
 });
