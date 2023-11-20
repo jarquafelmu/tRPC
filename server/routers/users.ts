429c5efcd01c4616a06d8f5a78b8590a24367d50
Sam Terrazas <thechaoselement@gmail.com>
2023-11-20 10:44:50 -0700

    Add zod

--- server/routers/users.ts
+++ server/routers/users.ts
@@ -1,10 +1,7 @@
 import { t } from "../trpc";
+import { z } from "zod";
 
-const userProcedure = t.procedure.input((v) => {
-  if (typeof v === "string") return v;
-
-  throw new Error("Invalid input: Expected a string");
-});
+const userProcedure = t.procedure.input(z.string());
 
 export const userRouter = t.router({
   getUser: userProcedure.query(() => ({ id: 1, name: "Kyle" })),
