a16d8edf19326445c304ea69fb1bc0a3b8ef76a9
Sam Terrazas <thechaoselement@gmail.com>
2023-11-20 10:34:26 -0700

    Nested router

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,4 +1,5 @@
 import { t } from "../trpc";
+import { userRouter } from "./users";
 
 export const appRouter = t.router({
   sayHi: t.procedure.query(() => {
@@ -14,4 +15,9 @@ export const appRouter = t.router({
       console.log(`Client says: ${req.input}`);
       return true;
     }),
+  // Nested router
+  // Add the user router to the app router
+  users: userRouter,
 });
+
+
