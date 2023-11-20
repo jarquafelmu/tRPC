dc28ec07cf3cddadcd14e50ab8a1afaaaab7603f
Sam Terrazas <thechaoselement@gmail.com>
2023-11-20 09:53:28 -0700

    Added tRPC to server

--- server/api.ts
+++ server/api.ts
@@ -1,7 +1,29 @@
 import express from "express";
 import cors from "cors";
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+
+const t = initTRPC.create();
+
+const appRouter = t.router({
+  sayHi: t.procedure.query(() => {
+    return "hi";
+  }),
+  logToServer: t.procedure
+    .input((v) => {
+      if (typeof v === "string") return v;
+
+      throw new Error("Invalid input: Expected a string");
+    })
+    .mutation((req) => {
+      console.log(`Client says: ${req.input}`);
+      return true;
+    }),
+});
 
 const app = express();
+app.use("/trpc", createExpressMiddleware({ router: appRouter }));
+
 app.use(cors({ origin: "http://localhost:5173" }));
 
 app.listen(3000);
