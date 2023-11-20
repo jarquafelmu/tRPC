016817cdaacbe5221c869547c51d787c4977573f
Sam Terrazas <thechaoselement@gmail.com>
2023-11-20 10:03:31 -0700

    put cors first

--- server/api.ts
+++ server/api.ts
@@ -22,10 +22,13 @@ const appRouter = t.router({
 });
 
 const app = express();
-app.use("/trpc", createExpressMiddleware({ router: appRouter }));
-
+// Allow CORS from the client
 app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 app.listen(3000);
 
 console.log("Server running on port 3000");
+
+export type AppRouter = typeof appRouter;
