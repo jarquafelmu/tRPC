a3e9ab7e4bb92c665694f2e7e1725c0138f1ed11
Sam Terrazas <thechaoselement@gmail.com>
2023-11-20 17:04:59 -0700

    using websockets on the client

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,19 +1,47 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import {
+  createTRPCProxyClient,
+  createWSClient,
+  wsLink,
+  splitLink,
+  httpBatchLink,
+} from "@trpc/client";
 import { AppRouter } from "../../server/api";
+
+const wsClient = createWSClient({
+  url: "ws://localhost:3000/trpc",
+});
 
 const client = createTRPCProxyClient<AppRouter>({
   links: [
-    httpBatchLink({
-      // sends all requests at once so less network usage for your consumer
-      url: "http://localhost:3000/trpc",
+    splitLink({
+      // Whenever we send a request, check if it's a subscription
+      condition: (op) => {
+        return op.type === "subscription";
+      },
+      // If it is a subscription, use the websocket link
+      true: wsLink({
+        client: wsClient,
+      }),
+      // If it is not a subscription, use the httpBatchLink
+      false: httpBatchLink({
+        // sends all requests at once so less network usage for your consumer
+        url: "http://localhost:3000/trpc",
+      }),
     }),
   ],
 });
 
+document.addEventListener("click", () => {
+  client.users.update.mutate({ userId: "1", name: "Kyle" });
+});
+
 async function main() {
-  // const result = await client.log.mutate(2); // This will throw an error
-  const result = await client.secretData.query();
-  console.log(result);
+  client.users.onUpdate.subscribe(undefined, {
+    onData: (id) => {
+      console.log(`User ${id} was updated`);
+    },
+  });
+  wsClient.close();
 }
 
 main().catch((err) => {
